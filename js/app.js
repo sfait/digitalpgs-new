63e1b20feff34b3dcd36b1c4771d848afd15898c
Sylwia Fait <sylwia.fait@gmail.com>
2019-10-24 13:09:58 +0200

    Add animation

--- js/app.js
+++ js/app.js
@@ -30,9 +30,46 @@ function showHamburgerMenu() {
     hamburger.addEventListener("click", handleClick);
 }
 
+function useScrollReveal() {
+    const slideLeft = {
+        distance: "150%",
+        origin: "left",
+        delay: "250",
+        opacity: null
+    };
+
+    const slideRight = {
+        distance: "150%",
+        origin: "right",
+        delay: "250",
+        opacity: null
+    };
+
+    ScrollReveal().reveal(".main-header, .navigation, .hero-wrapper", { delay: 250 });
+    ScrollReveal().reveal(".about-us, .contact", slideLeft);
+    ScrollReveal().reveal(".team", slideRight);
+    ScrollReveal().reveal(".footer-wrapper", { delay: 400 });
+}
+
+function showAnimations() {
+    const mobile = window.matchMedia("screen and (min-width: 1000px)");
+
+    if (mobile.matches) {
+        useScrollReveal();
+    }
+
+    mobile.addListener( function(mobile) {
+        if (mobile.matches) {
+            useScrollReveal();
+        }
+    });
+};
+
+
 const init = () => {
     changeTitle();
     showHamburgerMenu();
+    showAnimations();
 }
 
 document.addEventListener("DOMContentLoaded", init);
